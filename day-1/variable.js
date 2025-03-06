65ecfd6a50d5dc5d7b7cb08a200cd42d7bcd2dd4
Obayaa <doraappiah2004@gmail.com>
2025-03-06 19:26:55 +0000

    Created a variable.js file and declared variables with various datatypes

---
+++ day-1/variable.js
@@ -0,0 +1,14 @@
+/* declare variables and assign string, 
+boolean, undefined and null data types */
+
+// String
+let name = 'Dora'
+
+// Boolean
+let isMarried = false
+
+// Undefined
+let undefinedVar;
+
+// Null
+let nullVar  = null;
