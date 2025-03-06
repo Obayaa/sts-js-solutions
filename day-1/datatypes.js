432cc839b15a26bac7fbe103102ca64f923116c2
Obayaa <doraappiah2004@gmail.com>
2025-03-06 19:28:03 +0000

    Created datatypes.js file, declared series of variables and utilized the typoeof operator within it

---
+++ day-1/datatypes.js
@@ -0,0 +1,42 @@
+//Declared predefined variables for the typeof operator
+let myName = 'Dora';
+let isBool = true;
+let undefinedVar;
+let nullVar  = null;
+
+// Checking the Datatypes using the the typeof operator
+console.log(typeof myName);
+console.log(typeof isMarried);
+console.log(typeof undefinedVar);
+console.log(typeof nullVar)
+
+// Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
+// Declare four variables with assigned values
+let name = "Dora";
+let age = 15;
+let isMarried = false;
+let  school = "Slightly Techie School"
+
+// Declare variables to store data in multiple lines
+let first_name = "Dora";
+let last_name = "Appiah";
+let marital_status = "single";
+let country = "Ghana";
+let my_age = 13;
+
+// Declaring variables to store data on a single line
+let firstName = "Dora", lastName = "Appiah", maritalStatus = "single", myCountry = "Ghana", my_Age = 13;
+
+
+// Declaring two variables myAge and yourAge
+let myAge = 25;
+let yourAge = 30;
+
+// assigning the declared variables with initial values and logging to the browser console.
+console.log("I am ", myAge, " years old.")
+console.log("You are ", yourAge, " years old.")
